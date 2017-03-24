28b59ff046cbfdea333373db73a609385c90a6d0
Jorge Wong <JW@Jorges-MBP.attlocal.net>
2017-03-23 20:35:33 -0500

    comments

--- app/components/search/Query.js
+++ app/components/search/Query.js
@@ -2,80 +2,58 @@ var React = require('react');
 
 var Query = React.createClass({
 
-      getInitialState: function() {
-          return {topic: "eclipse", startYear: "2012", endYear: "2012"}
-      },
+    getInitialState: function() {
+        return {topic: "eclipse", startYear: "2012", endYear: "2012"}
+    },
 
-      handleChange: function(event) {
+    // listens for changes in the form input
+    handleChange: function(event) {
+        var newState = {};
+        newState[event.target.id] = event.target.value;
+        this.setState(newState);
+    },
 
-          var newState = {};
-          newState[event.target.id] = event.target.value;
-          this.setState(newState);
-      },
-
-      handleSubmit: function(event) {
+    // the button event on submit
+    handleSubmit: function(event) {
         event.preventDefault();
         // arguments/user-input are fed to searchQuery function and passed to parent
         this.props.searchQuery(this.state.topic, this.state.startYear, this.state.endYear);
-      },
+    },
 
-      render: function() {
-          return (
-              <div>
-                  <h3>Enter topic of interest, time interval, and submit to retrieve articles</h3>
-                  {/* here goes the request form */}
-                  <form onSubmit={this.handleSubmit}>
-                      <div className="form-group">
-                          <h4 className="">
-                              <strong>Topic</strong>
-                          </h4>
-                          <input
-                            value={this.state.topic}
-                            type="text"
-                            className="form-control"
-                            id="topic"
-                            onChange={this.handleChange}
-                            required
-                          />
+    render: function() {
+        return (
+            <div>
+                <h3>Enter topic of interest, time interval, and submit to retrieve articles</h3>
+                {/* here goes the request form */}
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <h4 className="">
+                            <strong>Topic</strong>
+                        </h4>
+                        <input value={this.state.topic} type="text" className="form-control" id="topic" onChange={this.handleChange} required/>
 
-                          <h4 className="">
-                              <strong>Start Year</strong>
-                          </h4>
-                          <input
-                            value={this.state.startYear}
-                            type="text"
-                            className="form-control"
-                            id="startYear"
-                            onChange={this.handleChange}
-                            required
-                          />
+                        <h4 className="">
+                            <strong>Start Year</strong>
+                        </h4>
+                        <input value={this.state.startYear} type="text" className="form-control" id="startYear" onChange={this.handleChange} required/>
 
-                          <h4 className="">
-                              <strong>End Year</strong>
-                          </h4>
-                          <input
-                            value={this.state.endYear}
-                            type="text"
-                            className="form-control"
-                            id="endYear"
-                            onChange={this.handleChange}
-                            required
-                          />
+                        <h4 className="">
+                            <strong>End Year</strong>
+                        </h4>
+                        <input value={this.state.endYear} type="text" className="form-control" id="endYear" onChange={this.handleChange} required/>
 
-                      </div>
+                    </div>
 
-                      <div className="pull-right">
-                          <button
-                            type="submit"
-                            className="btn btn-danger">
+                    <div className="pull-right">
+                        <button type="submit" className="btn btn-danger">
                             <h4>Submit</h4>
-                          </button>
-                      </div>
-                  </form>
+                        </button>
+                    </div>
+                </form>
 
-              </div>
-          )
-      }
-  });
+            </div>
+        );
+    }
+});
 
 module.exports = Query;
--- app/components/search/Results.js
+++ app/components/search/Results.js
@@ -1,57 +1,57 @@
 var React = require('react');
-
 var helpers = require("../utils/helpers");
 
 var Results = React.createClass({
 
-  getInitialState: function() {
-    return {
-      title: "",  // docs[0].headline.main
-      published: "",   // pub_date
-      url: ""     // web_url
-    }
-  },
+    // initializing variable states
+    getInitialState: function() {
+        return {
+            title: "",
+            published: "",
+            url: ""
+        }
+    },
 
-   displayArticles: function() {
-     return this.props.apiResults.arts.map( function(article, index) {
-       return (
-         <li key={index}>
-              <p>{article.headline.main}</p>
-              <a href={article.web_url} target="_blank">
-                <button className="btn btn-default ">View Article</button>
-              </a>
-              <a href={void(0)}>
-                {/* onClick will pass the current article as an argument to handleClick */}
-                <button
-                  id={"#" + index}
-                  className="btn btn-default"
-                  onClick={ function() { helpers.saveThisArticle({article}) }}>Save</button>
-              </a>
-              <p>Published on: {article.pub_date}</p>
-            </li>
+    // display the articles passed down from its parent Search
+    // map cycles through array of index-values where values are objects containing articles data
+    displayArticles: function() {
+        return this.props.apiResults.arts.map(function(article, index) {
+            return (
+                <li key={index}>
+                    <p>{article.headline.main}</p>
+                    <a href={article.web_url} target="_blank">
+                        <button className="btn btn-default ">View Article</button>
+                    </a>
+                    <a href={void(0)}>
+                        {/* onClick will request helper method to save the article in database */}
+                        <button id={"#" + index} className="btn btn-default" onClick={function() {
+                            helpers.saveThisArticle({article})
+                        }}>Save</button>
+                    </a>
+                    <p>Published on: {article.pub_date}</p>
+                </li>
             );
-     });
-   },
+        });
+    },
 
-     render: function() {
+    render: function() {
+        // message if there are no articles to display
+        if (!this.props.apiResults.arts) {
+            return (
+                <div>
+                    <h3>No Articles to be Displayed</h3>
+                </div>
+            );
+        }
 
-
-       if (!this.props.apiResults.arts) {
-      return (
-        <div>
-              <h3>No Articles to be Displayed</h3>
-        </div>
-      );
+        return (
+            <div className="panel-body">
+                <ul className="list-group">
+                    {this.displayArticles()}
+                </ul>
+            </div>
+        );
     }
-
-       return (
-         <div className="panel-body">
-           <ul className="list-group">
-             {this.displayArticles()}
-           </ul>
-         </div>
-       )
-     }
-   });
+});
 
 module.exports = Results;
